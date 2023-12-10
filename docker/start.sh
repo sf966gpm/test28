fad86bb6cf5ddec868ef9b4e1d163b1930677805
sf966gpm <mihailov.box@mail.ru>
2023-12-11 00:15:26 +0700

    fix: Добавил sleep 1 на всякий случай.

--- docker/start.sh
+++ docker/start.sh
@@ -4,6 +4,7 @@ app_dir=$1
 php=/usr/local/bin/php
 artisan="${app_dir}/artisan"
 
-sleep 5
+# На всякий случай, иногда не успевает встать postgres
+sleep 1
 $php "${artisan}" migrate --seed
 $php "${artisan}" serve --host=0.0.0.0 --port=8000
