1261868407ccb1fb61babafcda79c6781f6b0097
m.mihailov <m.mihailov@kmiac.ru>
2023-12-11 08:20:10 +0700

    refactor: Поменял внешние порты в docker-compose.

--- docker/start.sh
+++ docker/start.sh
@@ -5,6 +5,6 @@ php=/usr/local/bin/php
 artisan="${app_dir}/artisan"
 
 # На всякий случай, иногда не успевает встать postgres
-sleep 1
+sleep 5
 $php "${artisan}" migrate --seed
 $php "${artisan}" serve --host=0.0.0.0 --port=8000
